0fcc65eac29f210ce52ad92c6aebb848b50f6432
einar afiouni <einar.afiouni@bekk.no>
2020-11-30 08:41:53 +0100

    forgot the loading bar

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Loading.module.scss";
+
+const Loading: React.FC = () => {
+  return <div className={styles.loading}></div>;
+};
+
+export default Loading;
